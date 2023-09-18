7ef2972065ccb1f319d3efff296f9ee241fd7d18
Jamie Reason <95385925+jamiereason@users.noreply.github.com>
2023-09-18 18:22:12 +0100

    added adagrad algorithm

--- algorithms/adagrad.py
+++ algorithms/adagrad.py
@@ -1,3 +1,26 @@
 import numpy as np
 
 # AdaGrad Algorithm
+def adagrad(grad_fun, data, seed, learning_rate, batch_size=30, epsilon=1e-6, max_epochs=1000, return_trace=False):
+    n_obs = len(data)  # data is the training observations
+
+    w = np.array(seed)  # Set seed
+    trace = [w]  # Initialize trace
+    G = np.zeros_like(w)  # Initialize the accumulator matrix G
+
+    for epoch in range(max_epochs):
+        J = np.random.randint(0, n_obs, size=batch_size)  # Take a random subset of indices
+        batch = data[J, :]  # Sample the batch
+
+        grad = grad_fun(w, batch)  # Compute gradient
+
+        G = G + grad**2  # Update the accumulator matrix G with squared gradients
+        
+        w = w - (learning_rate / (np.sqrt(G) + epsilon)) * grad  # Update parameter w using AdaGrad formula
+        trace.append(w.copy())  # Append the current parameter value to the trace
+    
+    if return_trace:
+        return w, np.array(trace)
+    else:
+        return w
+    
